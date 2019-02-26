79164a5b39a6999b182d120bb007d68c8a906fdb
achelmo <arron.chelmo@gmail.com>
2019-02-25 20:55:13 -0600

    db connection established

--- src/gui/CurrentCalculator.java
+++ src/gui/CurrentCalculator.java
@@ -1,15 +1,34 @@
+/*
+ * Title:		CSIS 484 Current Calculator
+ * Author:		achelmo, bxlabz
+ * 
+ * Created:		2/18/19
+ * Modified:	2/25/19
+ * Description:	
+ * 
+ * 	
+ */
+
 package gui;
 
 import java.awt.Container;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
 //import java.util.Collections;
+import java.util.logging.Logger;
 
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTextArea;
+
+//import com.mysql.cj.xdevapi.Statement;
 
 
 public class CurrentCalculator extends JFrame{
@@ -27,6 +46,10 @@ public class CurrentCalculator extends JFrame{
 	private JTextArea linksTA;
 	
 	public CurrentCalculator() {
+		
+		
+		
+		
 		setTitle("Current Calculator");
 		setSize(WIDTH, HEIGHT);
 		
@@ -126,7 +149,10 @@ public class CurrentCalculator extends JFrame{
 				
 				"Yemen",
 				
-				"Zambia","Zimbabwe"};
+				"Zambia","Zimbabwe",
+				
+				"test",
+				"test1"};
 		
 		fromCB = new JComboBox <String> (countries);
 		toCB = new JComboBox <String> (countries);
@@ -181,6 +207,7 @@ public class CurrentCalculator extends JFrame{
 			}
 			else {
 				linksTA.setText("Top links for traveling from " + from + " to " + to);
+				//cc.createConnection();
 			} // end of else
 			//linksTA.append(from + "\n");
 			//linksTA.append(to);
@@ -193,7 +220,56 @@ public class CurrentCalculator extends JFrame{
 	
 	
 	public static void main(String[] args) {
+		//CurrentCalculator pro = new CurrentCalculator();
 		CurrentCalculator cc = new CurrentCalculator();
+		cc.createConnection();
+	}
+	
+	void createConnection() {
+		try {
+			//load the driver
+			Class.forName("com.mysql.jdbc.Driver");
+			//establish the connection
+			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/mydb","root","root");
+			
+			//create a statement object
+			java.sql.Statement stmt = con.createStatement();
+			
+			String a = "test";
+			String b = "test1";
+			
+			//create query
+			//ResultSet rs = ((java.sql.Statement) stmt).executeQuery("SELECT * FROM countrylinks");
+					//+ " WHERE countryFrom '"+ a +"' AND WHERE countryTo '" + b + "'");
+			
+			ResultSet rs = ((java.sql.Statement) stmt).executeQuery("SELECT * FROM countrylinks WHERE "
+					+ "countryFrom='"+ a +"'" + "AND countryTo='"+b+"'");
+			
+			
+			while(rs.next()) {
+				System.out.println("Links for traveling from " + rs.getString("countryFrom")+" to " +rs.getString("countryTo"));
+				String name = rs.getString("link1");
+				String name1 = rs.getString("link2");
+				String name2 = rs.getString("link3");
+				System.out.println(name);
+				System.out.println(name1);
+				System.out.println(name2);
+				System.out.println();
+			}
+			
+			System.out.println("Database Connection Successfull!");
+			
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			Logger.getLogger(CurrentCalculator.class.getName()).log(Level.SEVERE,null,e);
+			//e.printStackTrace();
+			System.out.println("fail1");
+		}
+		
+		catch (SQLException e) {
+			Logger.getLogger(CurrentCalculator.class.getName()).log(Level.SEVERE,null,e);
+			System.out.println("fail2");
+		}
 	}
 
 }
